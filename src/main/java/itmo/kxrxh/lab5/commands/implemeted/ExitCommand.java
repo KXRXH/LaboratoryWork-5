c516d2f2afada2998227dc4b5ff32c93e6eac15b
kxrxh <kxrxh.contact@gmail.com>
2023-03-03 10:29:40 +0300

    HUGE exit command logic improvement! (Now it uses isAnnotationPresent)

--- src/main/java/itmo/kxrxh/lab5/commands/implemeted/ExitCommand.java
+++ src/main/java/itmo/kxrxh/lab5/commands/implemeted/ExitCommand.java
@@ -3,9 +3,10 @@ package itmo.kxrxh.lab5.commands.implemeted;
 import itmo.kxrxh.lab5.collection.CollectionCore;
 import itmo.kxrxh.lab5.commands.Command;
 import itmo.kxrxh.lab5.commands.CommandBuilder;
-import itmo.kxrxh.lab5.commands.CommandInvoker;
 import itmo.kxrxh.lab5.commands.Executable;
+import itmo.kxrxh.lab5.utils.annotations.CollectionEditor;
 import itmo.kxrxh.lab5.utils.terminal.Colors;
+import itmo.kxrxh.lab5.utils.types.SizedStack;
 
 import java.util.Scanner;
 
@@ -16,12 +17,16 @@ import java.util.Scanner;
  */
 @Command(name = "Exit")
 public class ExitCommand implements Executable {
-    private final int commandHistorySize = CommandInvoker.getHistorySize();
+    private final SizedStack<Executable> commandHistory;
     private final CommandBuilder commandBuilder = CollectionCore.getCommandBuilder();
+
+    public ExitCommand(SizedStack<Executable> commandHistory) {
+        this.commandHistory = commandHistory;
+    }
 
     @Override
     public void execute() {
-        if (commandHistorySize >= 1) {
+        if (commandHistory.stream().anyMatch(c -> c.getClass().isAnnotationPresent(CollectionEditor.class))) {
             System.out.println(Colors.AsciiYellow + "Unsaved data can be lost!" + Colors.AsciiReset);
             System.out.print(Colors.AsciiYellow + "Save the collection before exiting (y/n): " + Colors.AsciiReset);
             Scanner scanner = new Scanner(System.in);
